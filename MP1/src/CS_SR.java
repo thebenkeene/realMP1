56c67fea66f8b8cf349b340bfae23c24ca367eea
sinclairjones95 <jonesuz@bc.edu>
2018-03-19 16:17:11 -0400

    read through config

--- MP1/src/CS_SR.java
+++ MP1/src/CS_SR.java
@@ -1,12 +1,35 @@
 import java.net.*;
 import java.io.*;
+import java.util.*;
 
 public class SocketTest {
+	private String min_delay;
+	private String max_delay;
+		
     public static void main(String[] args) throws IOException {
         
         startServer();
         startSender();
+        
         //loop
+        Scanner scanner = new Scanner(args);
+        if (scanner.hasNextLine()){
+        	String[] delays = scanner.nextLine().split("\\s");
+        	min_delay = delays[0];
+        	max_delay = delays[1];
+        }
+        	
+        
+        while(scanner.hasNextLine()){
+        	String line = scanner.nextLine();
+        	String[] tokens = line.split("\\s");
+        	
+        	String id = token[0];
+        	String ip = token[1];
+        	String port = token[2];
+        	
+        	//create new object with info
+        }
     }
     
     public static void startSender() {
@@ -15,7 +38,9 @@ public class SocketTest {
             public void run() {
                 try {
                     Socket s = new Socket(address, port);
-                  //finish
+                    
+                    //to do inputstream?
+                }
             }
         }).start();
     }
@@ -32,7 +57,7 @@ public class SocketTest {
                     Socket s = ss.accept();
                     
                     BufferedReader in = new BufferedReader(
-                                                           new InputStreamReader(s.getInputStream()));
+                                                           new InputStreamReader(s.getInputStream()) );
                     String line = null;
                     while ((line = in.readLine()) != null) {
                         System.out.println(line);
