52ad17742abab2ac6ca521cf212c568418bb5b6a
sinclairjones95 <jonesuz@bc.edu>
2018-03-19 20:30:00 -0400

    update socket manager

--- MP1/src/SocketManager.java
+++ MP1/src/SocketManager.java
@@ -9,13 +9,27 @@ import java.io.*;
     public class SocketManager
     {
         // initialize socket and input output streams
-        private Socket socket            = null;
-        private DataInputStream  input   = null;
-        private DataOutputStream out     = null;
+    	private List<Socket> list;
+    	//private List<SocketAddress> addresses;			//Not neaded
 
         // constructor to put ip address and port
         public Socketmanager()
         {
+        	
+        	list = new ArrayList<>();
+        	
+        }
+        
+        public void add(Socket socket){
+        	
+        	//Adds a connection from all the existings sockets to the new socket both ways
+        	for(Socket s: list){
+        		s.connect(socket.getLocalSocketAddress());		//connection from existing to new
+        		socket.connect(s.getLocalSocketAddress());		//connection from new to existing
+        	}
+        	
+        	list.add(socket);	//adds the socket to the list
+
         }
     }
 }
